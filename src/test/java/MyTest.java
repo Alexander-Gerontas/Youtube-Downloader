4a710f094d687db241b83ac52a60dce37205fbbc
Alex Gerontas <alex.gerontas@fantasysportsinteractive.co.uk>
2023-03-17 02:03:53 +0200

    disable test

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,4 +1,5 @@
 import com.yt.downloader.YT3;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -6,6 +7,7 @@ import java.io.IOException;
 public class MyTest
 {
     @Test
+    @Disabled
     public void successfullConvertion2() throws IOException
     {
         YT3 yt = new YT3();
